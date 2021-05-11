3d49d5c55b9d5a3ef00f77fd72d8f71d4a4533d5
jaguilarweb <jaguilarweb@users.noreply.github.com>
2021-05-11 01:20:57 +0000

    deploy: 8a4520e51a6625adb5f6111bec39e0ce9dd5fbfd

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,29 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+// This numbers are taken from the API.
+
+const minimun = 1;
+const maximum = 122;
+
+const random = () => Math.floor(Math.random() * (maximum - minimun)) + minimun
+
+const createImageNode = () => {
+  const container = document.createElement('div')
+  container.className = "p-4";
+
+  const imagen = document.createElement('img');
+  imagen.className = "mx-auto";
+  imagen.width = '320';
+  imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  container.appendChild(imagen);
+
+  return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode= document.getElementById('images');
+
+mountNode.append(nuevaImagen);
+
